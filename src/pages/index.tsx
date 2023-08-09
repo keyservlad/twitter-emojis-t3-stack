cc1d20480762497bc8a3e22e11af22a347651e8a
keyservlad <arnaud.guilhamat@isep.fr>
2023-08-09 20:39:20 +0200

    WIP : get data from author and display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,71 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
+import Image from "next/image";
+
+const CreatePostWizard = () => {
+  const { data: user } = useSession();
+
+  if (!user) return null;
+
+  console.log(user);
+
+  return (
+    <div className="flex w-full gap-3">
+      {!user.user.image ? (
+        <div className="relative h-14 w-14 rounded-full bg-slate-200" />
+      ) : (
+        <div className="relative h-14 w-14">
+          <Image
+            src={user.user?.image}
+            width={56}
+            height={56}
+            quality={100}
+            alt="user image"
+            className="rounded-full"
+          />
+        </div>
+      )}
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.image}
+        width={56}
+        height={56}
+        quality={100}
+        alt="author image"
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300">
+          <span>{`@${author.name}`}</span> ·{" "}
+          <span>{post.createdAt.toISOString()}</span>
+        </div>
+        <span className="">{post.content}</span>
+      </div>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
 
-  const { data } = api.posts.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <>
@@ -17,27 +76,27 @@ const Home: NextPage = () => {
         <meta name="description" content="twitter but only with emojis" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <p className="text-center text-2xl text-white">
-            {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-          </p>
-          <button
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-            onClick={sessionData ? () => void signOut() : () => void signIn()}
-          >
-            {sessionData ? "Sign out" : "Sign in"}
-          </button>
-        </div>
-        <div className="">
-          {data?.map((post) => (
-            <div
-              key={post.id}
-              className="flex flex-col items-center justify-center gap-4"
-            >
-              <p className="text-xl text-white">{post.content}</p>
-            </div>
-          ))}
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+          <div className="flex justify-center border-b border-slate-400 p-4">
+            {sessionData ? (
+              <CreatePostWizard />
+            ) : (
+              <button
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                onClick={
+                  sessionData ? () => void signOut() : () => void signIn()
+                }
+              >
+                {sessionData ? "Sign out" : "Sign in"}
+              </button>
+            )}
+          </div>
+          <div className="flex flex-col">
+            {data.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
+            ))}
+          </div>
         </div>
       </main>
     </>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -7,7 +8,38 @@ import {
 } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    // get all the users from prisma where post.authorId === user.id
+    const users = await ctx.prisma.user.findMany({
+      where: {
+        id: {
+          in: posts.map((post) => post.authorId),
+        },
+      },
+      take: 100,
+    });
+
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.name || !author.image) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for Post not found",
+        });
+      }
+      return {
+        post,
+        author: {
+          ...author,
+          name: author.name,
+        },
+      };
+    });
   }),
 });
