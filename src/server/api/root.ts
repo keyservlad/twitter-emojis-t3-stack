8b8ab788e4bf67d3f2b463837159789b261cd3a4
keyservlad <arnaud.guilhamat@isep.fr>
2023-09-07 11:51:46 +0200

    keep alive planetscale with cron job

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "./routers/posts";
 import { profileRouter } from "./routers/profile";
+import { keepAliveRouter } from "./routers/keep-alive";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { profileRouter } from "./routers/profile";
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
   profile: profileRouter,
+  keepAlive: keepAliveRouter,
 });
 
 // export type definition of API
